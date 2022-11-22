0041463711f36d75663c8afe54a56ad3453841d9
nia <niaramerquides@gmail.com>
2022-11-22 15:31:17 -0400

    adiciona mudanças no codigo até o modulo 04 funçoes com parâmetros

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
+// Pom
 function tocaSomPom() {
   document.querySelector("#som_tecla_pom").play();
 }
 
-document.querySelectorAll('.tecla')
+const listaDeTeclas = document.querySelectorAll(".tecla");
+
+let contador = 0;
+
+//enquanto
+while (contador < listaDeTeclas.length) {
+  listaDeTeclas[contador].onclick = tocaSomPom;
+
+  contador = contador + 1;
+
+  console.log(contador);
+}
